5ed090465a2ab9f26831bbb24f9e802280eef67a
apasquier00 <alexandre.pasquier@le-campus-numerique.fr>
2025-01-06 14:03:35 +0100

    exo 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,28 @@
 // you can write js here
 console.log('exo-2');
+var isTesting = false;
+
+if (isTesting){
+    console.log('exo-2');
+    var dateNow = Date.now();
+    var myDate = new Date(dateNow);
+}
+else{
+    var myDate = new Date('2025-01-06T09:16:00');
+}
+
+var dayMyDate = myDate.getDay();
+var hourMyDate = myDate.getHours();
+
+var msgWeek = 'nous sommes en semaine';
+var msgWeekEnd = 'nous sommes le Week End';
+console.log(hourMyDate);
+if (dayMyDate === 0 || dayMyDate === 6 || (dayMyDate === 5 && hourMyDate > 16) || (dayMyDate === 1 && hourMyDate < 9)){
+    console.log(msgWeekEnd);
+}
+else if (dayMyDate > 0 && dayMyDate < 6){
+    console.log(msgWeek);
+}
+else {
+    console.log('Erreur');
+}
