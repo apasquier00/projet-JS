54cd994be22fa6d231c447676f7a23da5f13a725
apasquier00 <alexandre.pasquier@le-campus-numerique.fr>
2025-01-06 16:47:57 +0100

    exo 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,11 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+secretMessage.pop();
+secretMessage.push('to','program');
+indexEasily = secretMessage.indexOf("easily");
+secretMessage[indexEasily] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(5, 5, 'know');
+console.log(secretMessage.join());
