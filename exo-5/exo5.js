05f42195317144f7b5756f6ea72586fe7b9fa2d8
apasquier00 <alexandre.pasquier@le-campus-numerique.fr>
2025-01-07 10:39:48 +0100

    exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,19 @@
 // you can write js here
-
 console.log('exo-5');
+let input = "Buongiorno, voi gioccare con me ?";
+
+const vowels = ["a", "e", "i", "o", "u", "y"];
+resultArray = [];
+
+for (let i = 0; i < input.length; i++){
+    for (let j = 0; j < vowels.length; j++){
+        if (input[i] === vowels[j]){
+            resultArray.push(input[i]);
+            break;
+        }
+    }
+
+}
+resultArray[0] = resultArray[0].toUpperCase();
+resultArray = resultArray.join('');
+console.log(resultArray);
